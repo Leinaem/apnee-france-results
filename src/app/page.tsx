e037c97290cec8762df012d37e2656f424179ac9
saggoune <saggoune@leparisien.fr>
2025-03-03 22:24:11 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <nav>
+        
+      </nav>
+      <main>
+       
+
+      </main>
+      <footer>
+
+
+      </footer>
+    </div>
+  );
+}
