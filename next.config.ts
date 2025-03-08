d222a9834093182f5e82bce436c7ea76ffbf9961
saggoune <saggoune@leparisien.fr>
2025-03-08 03:31:27 +0100

    Added, AWS dependencies, Axios, Uuix, config S3 and Dynamo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: [`${process.env.NEXT_PUBLIC_ASSET_DOMAIN}`],
+  },
 };
 
 export default nextConfig;
---
+++ src/utils/config.ts
@@ -0,0 +1,1 @@
+export const getAssetsUrl = (url: string): string => `${process.env.NEXT_PUBLIC_ASSET_URL}${url}`;
